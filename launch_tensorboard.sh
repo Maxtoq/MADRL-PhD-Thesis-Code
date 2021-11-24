ffb7f4f22f3a1d94bdf6d6c0956733fb74431abb
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2021-11-24 10:45:37 +0100

    add tensorboard script

---
+++ launch_tensorboard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script for launching the tensorboard server, listening on the port 8080
+echo Launching Tensorboard with command \"python venv/lib/python3.6/site-packages/tensorboard/main.py --logdir=models/ --port=8080 --bind_all\"
+
+source venv/bin/activate
+
+python venv/lib/python3.6/site-packages/tensorboard/main.py --logdir=models/ --port=8000 --bind_all
+
