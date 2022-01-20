87d43a0766b98a75da4e1aa8a5bd2376fbb47ee9
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2022-01-20 11:56:33 +0100

    add run

--- algorithms/MADDPG/train_run.sh
+++ algorithms/MADDPG/train_run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-n_run=2
+n_run=10
 env="coop_push_scenario/coop_push_scenario_closed.py"
 model_name="2addpg_fo_abs_cont_distrew"
 sce_conf_path="configs/2a_1o_fo_abs_distrew.json"
-n_episodes=500000
-n_exploration_eps=500000
-n_updates=3500
+n_episodes=300000
+n_exploration_eps=300000
+n_updates=20000
 lr=0.005
 hidden_dim=64
-n_rollout_threads=1
+n_rollout_threads=15
 
 for n in $(seq 1 $n_run)
 do
