da3c7c170d2ba3d457d497ff4fc3bfbd075755d1
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2022-11-09 11:50:05 +0100

    add run

--- algorithms/MALNovelD/qmix_train.sh
+++ algorithms/MALNovelD/qmix_train.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-n_run=4
+n_run=7
 env="algorithms/MALNovelD/scenarios/click_n_push2_fixed.py"
-model_name="qmix_panoveld_fo"
-sce_conf_path="configs/2a_1o_fo_rel.json"
+model_name="qmix_manoveld_po"
+sce_conf_path="configs/2a_1o_pol_rel.json"
 n_frames=10000000
 frames_per_update=100
 eval_every=1000000
 eval_scenar_file="eval_scenarios/hard_corners_24.json"
 init_explo_rate=0.3
 epsilon_decay_fn="linear"
-model_type="qmix_panoveld"
+model_type="qmix_manoveld"
 int_reward_coeff=1.0
 gamma=0.99
 embed_dim=16 # default 16
 nd_hidden_dim=64 # default 64
 nd_scale_fac=0.5 # default 0.5
 nd_lr=0.0001 # default 0.0001
-cuda_device="cuda:1"
+cuda_device="cuda:2"
 
 for n in $(seq 1 $n_run)
 do
