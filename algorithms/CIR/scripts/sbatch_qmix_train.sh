2f215527576375209ee0541fcad060ab0fc70b4d
Maxtoq <maxime.toquebiau@hotmail.fr>
2023-01-11 18:17:04 +0100

    add runs

--- algorithms/CIR/scripts/sbatch_qmix_train.sh
+++ algorithms/CIR/scripts/sbatch_qmix_train.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 #SBATCH --partition=hard
-#SBATCH --job-name=toquebiau
+#SBATCH --job-name=loc_corners
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
-#SBATCH --time=4600
+#SBATCH --time=600
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=maxime.toquebiau@sorbonne.universite.fr
 #SBATCH --output=outputs/%x-%j.out
 
 source venv/bin/activate
 
-n_run=11
-env="algorithms/MALNovelD/scenarios/coop_push_scenario_noshaping.py"
-model_name="qmix"
+n_run=1
+env="algorithms/MALNovelD/scenarios/coop_push_corners.py"
+model_name="qmix_loc_e2snoveld_invdyn"
 sce_conf_path="configs/2a_1o_fo_rel.json"
 n_frames=10000000
 n_explo_frames=10000000
+episode_length=100 # def 100
 frames_per_update=100
 eval_every=1000000
 eval_scenar_file="eval_scenarios/hard_corners_24.json"
 init_explo_rate=0.3
 epsilon_decay_fn="linear"
-intrinsic_reward_algo="none"
+intrinsic_reward_mode="local"
+intrinsic_reward_algo="e2snoveld"
 int_reward_coeff=1.0
 int_reward_decay_fn="constant"
 gamma=0.99
-int_rew_enc_dim=16 # default 16
-int_rew_hidden_dim=64 # default 64
-scale_fac=0.5 # default 0.5
-int_rew_lr=0.0001 # default 0.0001
+int_rew_enc_dim=64 # def 16
+int_rew_hidden_dim=128 # def 64
+scale_fac=0.5 # def 0.5
+int_rew_lr=0.0001 # def 0.0001
 state_dim=40
-optimal_diffusion_coeff=30
+optimal_diffusion_coeff=50
 cuda_device="cuda:0"
 
 for n in $(seq 1 $n_run)
@@ -38,9 +40,9 @@ do
     printf "Run ${n}/${n_run}\n"
     seed=$RANDOM
     comm="python algorithms/CIR/train_qmix.py --env_path ${env} --model_name ${model_name} --sce_conf_path ${sce_conf_path} --seed ${seed} \
---n_frames ${n_frames} --cuda_device ${cuda_device} --gamma ${gamma} --frames_per_update ${frames_per_update} \
+--n_frames ${n_frames} --cuda_device ${cuda_device} --gamma ${gamma} --episode_length ${episode_length} --frames_per_update ${frames_per_update} \
 --init_explo_rate ${init_explo_rate} --n_explo_frames ${n_explo_frames} \
---intrinsic_reward_algo ${intrinsic_reward_algo} \
+--intrinsic_reward_mode ${intrinsic_reward_mode} --intrinsic_reward_algo ${intrinsic_reward_algo} \
 --int_reward_coeff ${int_reward_coeff} --int_reward_decay_fn ${int_reward_decay_fn} \
 --scale_fac ${scale_fac} --int_rew_lr ${int_rew_lr} --int_rew_enc_dim ${int_rew_enc_dim} --int_rew_hidden_dim ${int_rew_hidden_dim} \
 --state_dim ${state_dim} --optimal_diffusion_coeff ${optimal_diffusion_coeff} --save_visited_states"
