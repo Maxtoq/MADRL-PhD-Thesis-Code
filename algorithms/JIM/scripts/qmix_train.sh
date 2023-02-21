234f7a157d794ab000d7e3ce1a5e97ab07b33ef7
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2023-02-21 17:49:24 +0100

    easier pb

--- algorithms/JIM/scripts/qmix_train.sh
+++ algorithms/JIM/scripts/qmix_train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 n_run=2
 env="algorithms/JIM/scenarios/push_buttons.py"
-model_name="qmix_lim_pol"
+model_name="qmix_jim_pol"
 sce_conf_path="configs/2a_pol.json"
 n_frames=10000000
 n_explo_frames=9000000
@@ -11,18 +11,18 @@ eval_every=10000
 eval_scenar_file="eval_scenarios/hard_corners_24.json"
 init_explo_rate=0.3
 epsilon_decay_fn="linear"
-intrinsic_reward_mode="local"
+intrinsic_reward_mode="central"
 intrinsic_reward_algo="e2snoveld"
 int_reward_coeff=1.0
 int_reward_decay_fn="constant"
 gamma=0.99
-int_rew_enc_dim=32 # def 16, JIM 90, LIM 30
-int_rew_hidden_dim=256 # def 64, JIM 1024, LIM 256
+int_rew_enc_dim=64 # def 16, JIM 90, LIM 30
+int_rew_hidden_dim=512 # def 64, JIM 1024, LIM 256
 scale_fac=0.5 # def 0.5
 int_rew_lr=0.0001 # def 0.0001
 state_dim=40
 optimal_diffusion_coeff=40
-cuda_device="cuda:0"
+cuda_device="cuda:3"
 
 for n in $(seq 1 $n_run)
 do
