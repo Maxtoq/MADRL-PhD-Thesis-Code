040b4da074b335526c923c81a965cf8ec768a8ae
Maxtoq <maxime.toquebiau@hotmail.fr>
2023-04-24 19:04:25 +0200

    add cp

--- algorithms/JIM/scripts/sbatch_qmix_train.sh
+++ algorithms/JIM/scripts/sbatch_qmix_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=hard
-#SBATCH --nodelist=top
-#SBATCH --job-name=4_corners
+#SBATCH --nodelist=lizzy
+#SBATCH --job-name=15_corners
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=7000
@@ -12,11 +12,11 @@
 source venv/bin/activate
 
 n_run=6
-env="algorithms/JIM/scenarios/coop_push_corners_new.py"
-model_name="qmix_4a_pol"
-sce_conf_path="configs/4a_pol.json"
-n_frames=10000000
-n_explo_frames=10000000
+env="algorithms/JIM/scenarios/coop_push_corners.py"
+model_name="qmix_jim_15"
+sce_conf_path="configs/2a_1o_fo.json"
+n_frames=15000000
+n_explo_frames=15000000
 episode_length=100 # def 100
 frames_per_update=100
 eval_every=10000
@@ -24,12 +24,12 @@ eval_scenar_file="eval_scenarios/hard_corners_24.json"
 init_explo_rate=0.3
 epsilon_decay_fn="linear"
 intrinsic_reward_mode="central"
-intrinsic_reward_algo="none"
-int_reward_coeff=0.3
+intrinsic_reward_algo="e2snoveld"
+int_reward_coeff=1.0
 int_reward_decay_fn="constant"
 gamma=0.99
-int_rew_enc_dim=120 # def 16, JIM 90, LIM 30
-int_rew_hidden_dim=1024 # def 64, JIM 1024, LIM 256
+int_rew_enc_dim=32 # def 16, JIM 90, LIM 30
+int_rew_hidden_dim=128 # def 64, JIM 1024, LIM 256
 scale_fac=0.5 # def 0.5
 int_rew_lr=0.0001 # def 0.0001
 state_dim=40
