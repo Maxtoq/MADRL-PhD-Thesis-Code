9508d0bd74df4e48cdbcb1a1e0f270330dda2fb7
Maxtoq <maxime.toquebiau@hotmail.fr>
2023-09-28 17:42:20 +0200

    rm cache

--- launch_tensorboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script for launching the tensorboard server, listening on the port 8080
-echo Launching Tensorboard with command \"python venv/lib/python3.6/site-packages/tensorboard/main.py --logdir=models/rel_overgen/ --port=8080 --bind_all\"
-
-source venv/bin/activate
-
-python venv/lib/python3.6/site-packages/tensorboard/main.py --logdir=models/rel_overgen/ --port=8080 --bind_all
-
