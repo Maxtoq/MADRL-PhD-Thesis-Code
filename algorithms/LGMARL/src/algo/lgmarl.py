2159447531f351c780d72e3944087c6fd2f48589
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2024-01-29 18:47:52 +0100

    fix ec

--- algorithms/LGMARL/src/algo/lgmarl.py
+++ algorithms/LGMARL/src/algo/lgmarl.py
@@ -179,15 +179,14 @@ class LanguageGroundedMARL:
             # Get lang contexts
             if self.comm_ec_strategy == "sum":
                 self.lang_contexts = self.comm_actions.sum(axis=1)
-                broadcasts = self.lang_contexts
             elif self.comm_ec_strategy == "mean":
                 self.lang_contexts = self.comm_actions.mean(axis=1)
-                broadcasts = self.lang_contexts
             elif self.comm_ec_strategy == "random":
                 rand_ids = np.random.randint(self.n_agents, size=self.n_envs)
                 self.lang_contexts = self.comm_actions[np.arange(self.n_envs), rand_ids]
             else:
                 raise NotImplementedError("Emergent communication strategy not implemented:", self.comm_ec_strategy)
+            broadcasts = self.lang_contexts
 
         elif self.comm_type == "perfect_comm":
             assert perfect_messages is not None
