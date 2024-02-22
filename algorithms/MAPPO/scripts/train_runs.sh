1db9e4d711210e1217eb85cd1ec8a96d11cf79bd
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2024-02-22 12:30:12 +0100

    ranger

--- algorithms/MAPPO/scripts/train_runs.sh
+++ algorithms/MAPPO/scripts/train_runs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 n_run=15
-experiment_name="mappo_2a_4030_LIM"
+experiment_name="mappo_2a_4040"
 n_rollout_threads=250
 n_steps=5000000
 algorithm_name="mappo"
@@ -8,13 +8,15 @@ ppo_epoch=15
 env_name="rel_overgen"
 ro_n_agents=2
 ro_state_dim=40
-ro_optim_diff_coeff=30.0
-ir_algo="e2s_noveld"
+ro_optim_diff_coeff=40.0
+ir_algo="none"
 ir_mode="local"
 ir_coeff=1.0
 ir_enc_dim=64
 ir_hidden_dim=128
-cuda_device="cuda:0"
+cuda_device="cuda:3"
+
+source venv3.8/bin/activate
 
 for n in $(seq 1 $n_run)
 do
