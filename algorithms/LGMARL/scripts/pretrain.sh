616d132f17b55a4a909d97eacd28d1e99246734b
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2024-02-24 11:28:40 +0100

    e

--- algorithms/LGMARL/scripts/pretrain.sh
+++ algorithms/LGMARL/scripts/pretrain.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 n_run=1
-experiment_name="ACC_9x9_language_FIXEDCAPTTRAIN"
+experiment_name="FIXED_ACC_9x9_perf"
 n_parallel_envs=250
 n_steps=10000000
 ppo_epoch=15 # default 15
+lr=0.0009 # default 0.0005
 n_mini_batch=1 # default 2
 entropy_coef=0.01 #default 0.01
 env_name="magym_PredPrey"
 episode_length=100
-comm_type="language" # default language
+comm_type="perfect_comm" # default language
 comm_ec_strategy="mean" # default sum
 comm_token_penalty=0.001
 context_dim=16 # default 16
 lang_clip_lr=0.0009 # default 0.007
 lang_clip_n_mini_batch=1 # default 2
 lang_clip_batch_size=256 # default 256
-lang_capt_lr=0.01 # default 0.007
+lang_capt_lr=0.007 # default 0.007
 lang_capt_lr_anneal_to=0.0001 # default 0.0001
 lang_capt_n_epochs=2 # default 2
 lang_capt_batch_size=10 # default 100
 magym_env_size=9
-cuda_device="cuda:3"
+cuda_device="cuda:1"
 
 source venv3.8/bin/activate
 
@@ -33,6 +34,7 @@ do
     --n_parallel_envs ${n_parallel_envs}\
     --n_steps ${n_steps}\
     --ppo_epoch ${ppo_epoch}\
+    --lr ${lr}\
     --n_mini_batch ${n_mini_batch}\
     --entropy_coef ${entropy_coef}\
     --env_name ${env_name}\
