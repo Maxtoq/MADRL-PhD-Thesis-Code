e7712580f904b455f61c66e445278fe90f3ebbd5
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2024-02-27 12:12:29 +0100

    add runs

--- algorithms/LGMARL/scripts/pretrain.sh
+++ algorithms/LGMARL/scripts/pretrain.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-n_run=3
-experiment_name="ACC_9x9_language"
+n_run=4
+experiment_name="ACC_9x9_no"
 n_parallel_envs=250
-n_steps=10000000
+n_steps=20000000
 hidden_dim=64 # default 64
 policy_recurrent_N=1 # default 1
 ppo_epoch=15 # default 15
 lr=0.0005 # default 0.0005
 n_mini_batch=1 # default 2
 entropy_coef=0.01 #default 0.01
-env_name="magym_PredPrey"
+env_name="magym_Foraging"
 episode_length=100
-comm_type="language" # default language
+comm_type="no_comm" # default language
 comm_ec_strategy="mean" # default sum
 comm_token_penalty=0.001
 context_dim=16 # default 16
@@ -23,7 +23,7 @@ lang_capt_lr_anneal_to=0.0001 # default 0.0001
 lang_capt_n_epochs=2 # default 2
 lang_capt_batch_size=10 # default 100
 magym_env_size=9
-cuda_device="cuda:1"
+cuda_device="cuda:0"
 
 source venv3.8/bin/activate
 
