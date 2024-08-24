01729e515294c7ca59716b6ce03638a1371572f4
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2024-08-24 12:48:46 +0200

    upd

--- algorithms/LGMARL_new/scripts/pretrain_diff.sh
+++ algorithms/LGMARL_new/scripts/pretrain_diff.sh
@@ -16,13 +16,13 @@ lang_batch_size=1024 # default 256
 lang_capt_loss_weight=1 # default 0.0001
 lang_embed_dim=4 # default 4
 
-n_run=3
-experiment_name="9o5_Diff_noc"
+n_run=1
+experiment_name="9o5_Diff_perf_lastadvcomm"
 lr=0.0005 # default 0.0005
 entropy_coef=0.01 #default 0.01
-comm_type="no_comm" # default language
+comm_type="perfect" # default language
 context_dim=16 # default 16
-cuda_device="cuda:1"
+cuda_device="cuda:3"
 
 magym_env_size=9
 magym_obs_range=5 # default 5
--- algorithms/LGMARL_new/scripts/sbatch_train_lgmarl_diff.sh
+++ algorithms/LGMARL_new/scripts/sbatch_train_lgmarl_diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=hard
-#SBATCH --job-name=noc
+#SBATCH --job-name=ec
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=5000
@@ -10,31 +10,34 @@
 
 source venv/bin/activate
 
-n_run=4
-experiment_name="9o5_Diff_nocom"
 n_parallel_envs=250
 n_steps=10000000
 hidden_dim=64 # default 64
 policy_recurrent_N=1 # default 1
 ppo_epoch=15 # default 15
-lr=0.0005 # default 0.0005
 rollout_length=100 # default 100
 n_mini_batch=1 # default 2
-entropy_coef=0.01 #default 0.01
-env_name="magym_PredPrey"
+env_name="magym_PredPrey_new"
 episode_length=100
-comm_type="no_comm" # default language
-comm_ec_strategy="mean" # default sum
+comm_ec_strategy="cat" # default sum
 comm_eps_smooth=2.0 # default 1.0
 comm_token_penalty=0.001
-context_dim=16 # default 16
 lang_lr=0.005 # default 0.007
 lang_batch_size=1024 # default 256
 lang_capt_loss_weight=1 # default 0.0001
 lang_embed_dim=4 # default 4
+
+n_run=8
+experiment_name="9o5_Diff_ec8"
+lr=0.0005 # default 0.0005
+entropy_coef=0.01 #default 0.01
+comm_type="emergent_continuous" # default language
+context_dim=8 # default 16
+cuda_device="cuda:0"
+
 magym_env_size=9
 magym_obs_range=5 # default 5
-cuda_device="cuda:0"
+magym_n_agents=4
 
 for n in $(seq 1 $n_run)
 do
@@ -64,9 +67,11 @@ do
     --lang_capt_loss_weight ${lang_capt_loss_weight}
     --magym_env_size ${magym_env_size}
     --magym_obs_range ${magym_obs_range}
+    --magym_n_agents ${magym_n_agents}
     --dyna_weight_loss"
+    # --share_params"
+    # --lang_imp_sample"
     # --log_comm"
-    # --share_params\
     # --no_comm_head_learns_rl"
     printf "Starting training with command:\n${comm}\n\nSEED IS ${seed}\n"
     eval $comm
