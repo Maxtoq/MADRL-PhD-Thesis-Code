ba7e2b67249db5ef8e64cc0cf29c3d4ae8ae8cf5
Maxtoq <maxime.toquebiau@hotmail.fr>
2024-09-10 21:08:14 +0200

    del cp

--- algorithms/LGMARL_new/scripts/sbatch_train_lgmarl_diff.sh
+++ algorithms/LGMARL_new/scripts/sbatch_train_lgmarl_diff.sh
@@ -27,7 +27,7 @@ lang_capt_loss_weight=1 # default 0.0001
 lang_embed_dim=4 # default 4
 
 n_run=7
-experiment_name="9o5SA_Diff_edl"
+experiment_name="9o5SA_Diff_ec2"
 lr=0.0005 # default 0.0005
 entropy_coef=0.01 #default 0.01
 comm_type="emergent_discrete_lang" # default language
