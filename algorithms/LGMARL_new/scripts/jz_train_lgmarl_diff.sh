3d7371bd85f987fc4b1b4c538ef118b6dfb45130
Maxtoq <maxime.toquebiau@hotmail.fr>
2024-09-12 10:41:36 +0200

    atdé

--- algorithms/LGMARL_new/scripts/jz_train_lgmarl_diff.sh
+++ algorithms/LGMARL_new/scripts/jz_train_lgmarl_diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=gpu_p2
-#SBATCH --job-name=ec
+#SBATCH --job-name=noc
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1 
 #SBATCH --time=20:00:00
@@ -27,11 +27,11 @@ lang_capt_loss_weight=1 # default 0.0001
 lang_embed_dim=4 # default 4
 lang_hidden_dim=64
 
-n_run=1
-experiment_name="9o5_Diff_langsup"
+n_run=3
+experiment_name="9o5_noc"
 lr=0.0005 # default 0.0005
 entropy_coef=0.01 #default 0.01
-comm_type="language_sup" # default language
+comm_type="no_comm" # default language
 context_dim=16 # default 16
 lang_lr=0.007 # default 0.007
 cuda_device="cuda:0"
