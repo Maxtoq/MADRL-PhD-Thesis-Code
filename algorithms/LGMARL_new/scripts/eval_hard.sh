f9e6898b15fd56a6783dcda7458724875e895910
Maxime TOQUEBIAU <maxime.toquebiau@hotmail.fr>
2024-09-19 13:48:16 +0200

    atd nl obs

--- algorithms/LGMARL_new/scripts/eval_hard.sh
+++ algorithms/LGMARL_new/scripts/eval_hard.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
-n_parallel_envs=24 # 24 is number of hard scenarios
+n_parallel_envs=240 # 24 is number of hard scenarios
 eval_scenario="algorithms/LGMARL_new/src/envs/magym_PredPrey/hard_eval.json"
 n_eval_runs=1000
-cuda_device="cuda:2"
+cuda_device="cuda:3"
 
-rollout_length=40
-episode_length=40
+model_dir="models/magym_PredPrey_new/9o5_noc/run3,models/magym_PredPrey_new/9o5_noc/run3,models/magym_PredPrey_new/9o5_noc/run6,models/magym_PredPrey_new/9o5_noc/run6"
+# model_dir="models/magym_PredPrey_new/9o5_Diff_perf/run3,models/magym_PredPrey_new/9o5_Diff_perf/run3,models/magym_PredPrey_new/9o5_Diff_perf/run9,models/magym_PredPrey_new/9o5_Diff_perf/run9"
+# model_dir="models/magym_PredPrey_new/9o5_Diff_langsup/run10,models/magym_PredPrey_new/9o5_Diff_langsup/run10,models/magym_PredPrey_new/9o5_Diff_langsup/run3,models/magym_PredPrey_new/9o5_Diff_langsup/run3"
+# model_dir="models/magym_PredPrey_new/9o5_Diff_ec2/run9,models/magym_PredPrey_new/9o5_Diff_ec2/run9,models/magym_PredPrey_new/9o5_Diff_ec2/run5,models/magym_PredPrey_new/9o5_Diff_ec2/run5"
 
-# model_dir="models/magym_PredPrey_new/9o5_noc/run3" #,models/magym_PredPrey_new/9o5_noc/run3,models/magym_PredPrey_new/9o5_noc/run3,models/magym_PredPrey_new/9o5_noc/run6"
-# model_dir="models/magym_PredPrey_new/9o5_Diff_perf/run3,models/magym_PredPrey_new/9o5_Diff_perf/run3,models/magym_PredPrey_new/9o5_Diff_perf/run3,models/magym_PredPrey_new/9o5_Diff_perf/run9"
-model_dir="models/magym_PredPrey_new/9o5_Diff_langsup/run10" # ,models/magym_PredPrey_new/9o5_Diff_langsup/run10,models/magym_PredPrey_new/9o5_Diff_langsup/run10,models/magym_PredPrey_new/9o5_Diff_langsup/run3"
-# model_dir="models/magym_PredPrey_new/9o5_Diff_ec2/run9,models/magym_PredPrey_new/9o5_Diff_ec2/run9,models/magym_PredPrey_new/9o5_Diff_ec2/run9,models/magym_PredPrey_new/9o5_Diff_ec2/run5"
-
-seed=$RANDOM
+seed=1000
 comm="python algorithms/LGMARL_new/eval_zst.py 
     --seed ${seed}
     --cuda_device ${cuda_device}
@@ -20,9 +17,7 @@ comm="python algorithms/LGMARL_new/eval_zst.py
     --n_steps 100 
     --eval_scenario ${eval_scenario} 
     --n_eval_runs ${n_eval_runs}
-    --n_parallel_envs ${n_parallel_envs}
-    --rollout_length ${rollout_length}
-    --episode_length ${episode_length}"
+    --n_parallel_envs ${n_parallel_envs}"
     # --use_render
     # --render_wait_input"
 echo $comm
